34ec09d5d36e84ea3ad5b3d1afd434682d627c64
sara <naifarsara@gmail.com>
2017-12-19 15:52:05 +0100

    adding server.js file for server test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express= require('express')
+const logger = require('morgan')
+const errorhandler = require ('errorhandler')
+const mongodb= require('mongodb')
+
+
+
+let app  = express()
+
+app.use(logger('dev'))
+app.use(errorhandler())
+
+app.get('/login' , (req,res)=>{
+   res.send('this is the Home page')
+})
+
+
+app.listen(8000 , ()=>{console.log('server listening in 8000')})
